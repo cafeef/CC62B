78256057b7509e3281d26e4f44aabf9d633e6174
Fernanda Pacheco <cafeef16@gmail.com>
2024-10-02 10:19:15 -0300

    adicionei o primeiro exercício e suas configuraçõeses

--- revisao/ex001.c
+++ revisao/ex001.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
 int main() {
-    printf("hello world");
+    int horas, minutos, total;
+    setlocale(LC_ALL, "Portuguese_Brasil");
+    puts("Digite as horas: ");
+    scanf("%d", &horas);
+    puts("Digite os minutos: ");
+    scanf("%d", &minutos);
+    total = horas * 60 + minutos;
+    printf("O total de minutos é: %d", total);
 }
