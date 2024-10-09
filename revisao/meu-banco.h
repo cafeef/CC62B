3e606f4023f3f1cb107148395deb42f04ff9e635
Fernanda Pacheco <cafeef16@gmail.com>
2024-10-09 17:58:05 -0300

    adicionei a biblioteca do banco

---
+++ revisao/meu-banco.h
@@ -0,0 +1,4 @@
+void incluir(int vcod[], float vsaldo[], int tam);
+void deposito(int cod, float vsaldo[], float valor);
+void saque(int cod_cliente, float vsaldo[], float valor);
+float versaldo(int cod_cliente, float vsaldo[]);
