d08901767a6f47b3d8fe45166332e1e5a3e6745a
Fernanda Pacheco <109240203+cafeef@users.noreply.github.com>
2024-10-10 11:49:08 -0300

    upload desafio bib

---
+++ revisao/desafio-bib/main.c
@@ -0,0 +1,4 @@
+//criar uma bib que tenhas as f
+//1) buscar um caractere em uma string, retornar a posição do caractere na string
+//2)contar o numero de caracteres de uma string
+//3)concatenar duas strings
