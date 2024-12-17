8703760c9443460c342bf6556349d2e7a1e6db6f
Fernanda Pacheco <cafeef16@gmail.com>
2024-12-17 11:59:30 -0300

    coloquei transferencias de pilha e fila

--- listas-encadeadas/pilha/ex001.c
+++ listas-encadeadas/pilha/ex001.c
@@ -6,6 +6,11 @@ typedef struct No {
     struct No *prox;
 } TNo;
 
+typedef struct cabeca {
+    TNo *inicio;
+    TNo *fim;
+} TCabeca;
+
 TNo *alocaNo(int k) {
     TNo *novo = (TNo *) malloc(sizeof(TNo));
     if (novo) {
@@ -45,9 +50,94 @@ void imprimir(TNo *p) {
     else printf("Pilha vazia!\n");
 }
 
+void transfere(TNo **pa, TNo **pb) {
+    if (!*pa) printf("Pilha A vazia!\n");
+    else {
+        while (*pa) { //transfere todos
+            empilhar(pb, (*pa)->chave);
+            desempilhar(pa);
+        }
+    }
+}
+
+TCabeca *criacabeca() {
+    TCabeca *lista = (TCabeca *) malloc(sizeof(TCabeca));
+    if (lista) {
+        lista->inicio = NULL;
+        lista->fim = NULL;
+        return lista;
+    }
+    else return NULL;
+}
+
+void enfileirar(TCabeca *p, int k) {
+    TNo *novo = (TNo *) malloc(sizeof(TNo));;
+    if (novo) {
+        novo->chave = k;
+        if (!p->inicio) {
+            p->inicio = novo;
+            p->fim = novo;
+        }   
+        else
+            p->fim->prox = novo;
+            p->fim = novo;
+
+    }
+}
+
+void desenfileirar(TCabeca *p) {
+    if (p) {
+        TNo *aux = p->inicio;
+        p->inicio = aux->prox;
+        if (!p->inicio)
+            p->fim = NULL;
+        free(aux);
+        aux = NULL;
+    }
+    else printf("Não existe nó cabeça!\n");
+}
+
+void FilhaPilha(TCabeca *fila, TNo **pilha) {
+    if (fila->inicio) {
+        while (fila->inicio) {
+            empilhar(pilha, fila->inicio->chave);
+            desenfileirar(fila);
+        }
+        printf("Transferência completa\n");
+    }
+    else {
+        printf("Não tem fila...\n");
+        return;
+    }
+}
+
+void PilhaFila (TCabeca *fila, TNo **pilha) {
+    if (*pilha) {
+        while (*pilha) {
+            enfileirar(fila, (*pilha)->chave);
+            desempilhar(pilha);
+        }
+    }
+    else {
+        printf("Pilha vazia!\n");
+        return;
+    }
+}
+
 int main() {
-    TNo *lista = NULL;
-    desempilhar(&lista);
-    empilhar(&lista, 16);
-    imprimir(lista);
+    TNo *topoA = NULL, *topoB = NULL;
+    TCabeca *fila = criacabeca();
+    enfileirar(fila, 56);
+    enfileirar(fila, 73);
+    enfileirar(fila, 92);
+    enfileirar(fila, 84);
+    imprimir(fila->inicio);
+    printf("\n");
+    empilhar(&topoA, 1);
+    empilhar(&topoA, 2);
+    empilhar(&topoA, 3);
+    empilhar(&topoA, 4);
+    PilhaFila(fila, &topoA);
+    imprimir(fila->inicio);
+    printf("\n");
 }
