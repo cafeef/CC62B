b0dba1b8fc7986a6ad7cd3de6de27ac624062867
Fernanda Pacheco <cafeef16@gmail.com>
2024-12-17 10:55:40 -0300

    criei as rotinas básicas

---
+++ listas-encadeadas/pilha/ex001.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No {
+    int chave;
+    struct No *prox;
+} TNo;
+
+TNo *alocaNo(int k) {
+    TNo *novo = (TNo *) malloc(sizeof(TNo));
+    if (novo) {
+        novo->chave = k;
+        novo->prox = NULL;
+        return novo;
+    }
+    else return NULL;
+}
+
+void empilhar(TNo **p, int k) { //push
+	TNo *novo = alocaNo(k);
+	if (novo) {
+		novo->prox = *p; //primeiro elemento da lista
+		*p = novo;
+	}
+    else return;
+}
+
+void desempilhar(TNo **p) { //pop
+    if (*p) {
+        TNo *aux = *p;
+        *p = (*p)->prox;
+        free(aux);
+        aux = NULL;
+    }
+    else printf("Lista vazia!\n");
+}
+
+void imprimir(TNo *p) {
+    if (p) {
+        while (p) {
+            printf("%d ", p->chave);
+            p = p->prox;
+        }
+    }
+    else printf("Lista vazia!\n");
+}
+
+int main() {
+    TNo *lista = NULL;
+    empilhar(&lista, 10);
+    empilhar(&lista, 18);
+    desempilhar(&lista);
+    empilhar(&lista, 16);
+    imprimir(lista);
+}
